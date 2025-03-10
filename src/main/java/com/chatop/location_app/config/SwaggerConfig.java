4716227141ff537a1785980ed907c32d133a1917
Dorian <dorianpernot2@gmail.com>
2025-03-10 20:56:22 +0100

    maj swagger

--- src/main/java/com/chatop/location_app/config/SwaggerConfig.java
+++ src/main/java/com/chatop/location_app/config/SwaggerConfig.java
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 @SecurityScheme(name = "Bearer Authentication", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
@@ -17,23 +15,11 @@ public class SwaggerConfig {
 
   @Bean
   public GroupedOpenApi api() {
-    return GroupedOpenApi.builder()
-            .group("app")
-            .packagesToScan("com.chatop.location_app.controller")
-            .pathsToMatch("/**")
-            .build();
+    return GroupedOpenApi.builder().group("app").packagesToScan("com.chatop.location_app.controller").pathsToMatch("/**").build();
   }
 
   @Bean
   public OpenAPI springShopOpenAPI() {
-    return new OpenAPI()
-            .info(new Info().title("location app - Chatop API")
-                    .description("location app - Chatop API")
-                    .version("v1.0.0")
-                    .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-            .externalDocs(new ExternalDocumentation()
-                    .description("location app - Chatop API Wiki Documentation")
-                    .url("https://springshop.wiki.github.org/docs"));
+    return new OpenAPI().info(new Info().title("location app - Chatop API").description("location app - Chatop API").version("v1.0.0"));
   }
-
 }
